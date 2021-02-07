d0ff0c451cf32b2e2d2ee7baf60892a28ab4a037
unknown <cuevastl@gmail.com>
2021-02-07 12:50:03 -0500

    Created Exercise class and tests, started work on user interface.

---
+++ Exercise.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Choose_Your_Class
+{
+    public class Exercise
+    {
+        public string Name { get; set; }
+        public int RepsPerSet { get; set; } = 10;
+        public string MuscleWorked { get; set; }
+        public int NumberOfSets { get; set; } = 1;
+        public int WeightUsed { get; set; }
+        public int MuscleExaustion { get; set; } = 20;
+
+        public Exercise()
+        {
+
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetRepsPerSet(int repsPerSet)
+        {
+            RepsPerSet = repsPerSet;
+        }
+
+        public int GetRepsPerSet()
+        {
+            return RepsPerSet;
+        }
+
+        public void SetMuscleWorked(string muscleWorked)
+        {
+            MuscleWorked = muscleWorked;
+        }
+
+        public string GetMuscleWorked()
+        {
+            return MuscleWorked;
+        }
+
+        public void SetNumberOfSets(int numberOfSets)
+        {
+            NumberOfSets = numberOfSets;
+        }
+
+        public int GetNumberOfSets()
+        {
+            return NumberOfSets;
+        }
+
+        public void SetWeightUsed(int weightUsed)
+        {
+            WeightUsed = weightUsed;
+        }
+
+        public int GetWeightUsed()
+        {
+            return WeightUsed;
+        }
+
+        public void IncreaseDifficulty()
+        {
+            RepsPerSet += 2;
+
+            NumberOfSets += 1;
+        }
+
+        public void DecreaseDifficulty()
+        {
+            RepsPerSet -= 2;
+
+            NumberOfSets -= 1;
+        }
+
+        public void IncreaseWeight()
+        {
+            WeightUsed += 5;
+        }
+
+        public void DecreaseWeight()
+        {
+            WeightUsed -= 5;
+        }
+
+        public int CalculateMuscleExaustion()
+        {
+            MuscleExaustion *= NumberOfSets;
+
+            return MuscleExaustion;
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace Choose_Your_Class
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello! Let's Build a Workout!");
+
+
+            bool keepPlaying = true;
+            while (keepPlaying)
+            {
+                Console.WriteLine("1. Add Exercise From Library");
+                Console.WriteLine("2. Create New Exercise");
+                Console.WriteLine("3. View Workout");
+                Console.WriteLine("4. Increase Difficulty of Workout");
+                Console.WriteLine("5. Decrease Difficulty of Workout");
+                Console.WriteLine("6. Select Exercise in Workout to Edit");
+
+                string menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
